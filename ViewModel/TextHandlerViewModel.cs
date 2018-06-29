de53fcb26242d9c1d7850f2e5f11f3e0a931e780
Porkki <porkki123@gmail.com>
2018-06-29 14:30:54 +0300

    Removed unnecessary using lines

--- ViewModel/TextHandlerViewModel.cs
+++ ViewModel/TextHandlerViewModel.cs
@@ -1,14 +1,6 @@
-﻿using Microsoft.Win32;
-using SimpleTextEditor.Command;
+﻿using SimpleTextEditor.Command;
 using SimpleTextEditor.Model;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace SimpleTextEditor.ViewModel
 {
