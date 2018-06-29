fb895523e500d0a694673ecf274f06147a9ad11a
Porkki <porkki123@gmail.com>
2018-06-29 14:27:07 +0300

    Made changes for generalized button commands and moved button interaction logic away from viewmodel to model

--- ViewModel/TextHandlerViewModel.cs
+++ ViewModel/TextHandlerViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using SimpleTextEditor.Command;
 using SimpleTextEditor.Model;
 using System;
 using System.Collections.Generic;
@@ -17,17 +18,29 @@ namespace SimpleTextEditor.ViewModel
         public TextHandlerModel handlerModel { get; set; }
 
         //Commands (for buttons etc)
-        public ICommand OpenCommand { get; private set; }
-        public ICommand SaveAsCommand { get; private set; }
-        public ICommand SaveCommand { get; private set; }
+        public ButtonCommand OpenCommand { get; private set; }
+        public ButtonCommand SaveAsCommand { get; private set; }
+        public ButtonCommand SaveCommand { get; private set; }
 
         public TextHandlerViewModel()
         {
             //Assigning handerlModel new instance of TextHandlerModel
             handlerModel = new TextHandlerModel();
-            OpenCommand = new FileOpenCommand(this);
-            SaveAsCommand = new FileSaveAsCommand(this);
-            SaveCommand = new FileSaveCommand(this);
+            //We need to listen the modifications what happen in the model so we can tell the ui to update
+            handlerModel.PropertyChanged += HandlerModel_PropertyChanged;
+            //Buttons
+            OpenCommand = new ButtonCommand(handlerModel.OpenExecute, handlerModel.OpenIsValid);
+            SaveAsCommand = new ButtonCommand(handlerModel.SaveAsExecute, handlerModel.SaveAsIsValid);
+            SaveCommand = new ButtonCommand(handlerModel.SaveExecute, handlerModel.SaveIsValid, this);
+        }
+
+        private void HandlerModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //If we receive information that the TextContent is changed then we will update it in the viewmodel
+            if(e.PropertyName == "TextContent")
+            {
+                TextContent = handlerModel.TextContent;
+            }
         }
 
         //Getting and setting TextContent to handlerModel
@@ -63,111 +76,6 @@ namespace SimpleTextEditor.ViewModel
         private void RaisePropertyChanged(string property)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-        }
-
-    }
-
-    class FileSaveCommand : ICommand
-    {
-        //We make new ViewModel object called parent
-        TextHandlerViewModel parent;
-
-        public FileSaveCommand(TextHandlerViewModel parent)
-        {
-            //Assigns FileOpenCommand parent to TextHandlerViewModel so we can access the data
-            this.parent = parent;
-            //This is black magic
-            parent.PropertyChanged += delegate { CanExecuteChanged?.Invoke(this, EventArgs.Empty); };
-        }
-
-        public event EventHandler CanExecuteChanged;
-
-        public bool CanExecute(object parameter)
-        {
-            //We grey out the button if user has not opened any files
-            if (parent.FilePath == string.Empty || parent.FilePath == null)
-            {
-                return false;
-            } else
-            {
-                return true;
-            }
-        }
-
-        public void Execute(object parameter)
-        {
-            File.WriteAllText(parent.FilePath, parent.TextContent);
-        }
-
-    }
-
-    class FileSaveAsCommand : ICommand
-    {
-        //We make new ViewModel object called parent
-        TextHandlerViewModel parent;
-
-        public FileSaveAsCommand(TextHandlerViewModel parent)
-        {
-            //Assigns FileOpenCommand parent to TextHandlerViewModel so we can access the data
-            this.parent = parent;
-            //This is black magic
-            parent.PropertyChanged += delegate { CanExecuteChanged?.Invoke(this, EventArgs.Empty); };
-        }
-
-        public event EventHandler CanExecuteChanged;
-
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
-
-        public void Execute(object parameter)
-        {
-            //Creates new SaveFileDialog called saveFile
-            SaveFileDialog saveFile = new SaveFileDialog();
-
-            //Opens the file dialog and saves the file to user specified location
-            if (saveFile.ShowDialog() == true)
-            {
-                File.WriteAllText(saveFile.FileName, parent.TextContent);
-            }
-        }
-
-    }
-
-    class FileOpenCommand : ICommand
-    {
-        //We make new ViewModel object called parent
-        TextHandlerViewModel parent;
-        
-        public FileOpenCommand(TextHandlerViewModel parent)
-        {
-            //Assigns FileOpenCommand parent to TextHandlerViewModel so we can access the data
-            this.parent = parent;
-            //This is black magic
-            parent.PropertyChanged += delegate { CanExecuteChanged?.Invoke(this, EventArgs.Empty); };
-        }
-
-        public event EventHandler CanExecuteChanged;
-
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
-
-        public void Execute(object parameter)
-        {
-            //Creates new OpenFileDialog called openFile
-            OpenFileDialog openFile = new OpenFileDialog();
-
-            /*Opens the file dialog and sets the ViewModels 
-             * textcontent to whatever file user decides to open */
-            if (openFile.ShowDialog() == true)
-            {
-                parent.TextContent = File.ReadAllText(openFile.FileName);
-                //We save the path so we can enable the save button
-                parent.FilePath = openFile.FileName;
-            }
         }
 
     }
