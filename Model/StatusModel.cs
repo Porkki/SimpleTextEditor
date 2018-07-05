ca490a83a7dc78b50862fe46db99f9164a334c33
Porkki <porkki123@gmail.com>
2018-07-05 20:33:46 +0300

    Created Status handler class

---
+++ Model/StatusModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTextEditor.Model
+{
+    class StatusModel : INotifyPropertyChanged
+    {
+        private string _Message;
+        public string Message
+        {
+            get
+            {
+                return _Message;
+            }
+            set
+            {
+                if (value != _Message)
+                {
+                    _Message = value;
+                    RaisePropertyChanged("Status");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
